ff67a84b477c9405027f3b294f3e4de4d60ec955
Rodrigo Scalco <rodrigo@bling.com.br>
2013-12-09 09:52:35 -0200

    File03 updated.

--- File01.php
+++ File01.php
@@ -2,4 +2,5 @@
 	echo "File01 - Master inicial";
 	echo "File01 - version: Branch01 - status: Commited";
 	echo "File01 - version: Branch01 - status: Commited";
+	echo "File01 - version: Branch01 - status: Commited";
 ?>
