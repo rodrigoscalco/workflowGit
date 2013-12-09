9a78f7cc60da6e21a3d96046994ade7cb92e26b3
rodrigoscalco <rodrigo@bling.com.br>
2013-12-09 10:01:16 -0200

    Update File01.php

--- File01.php
+++ File01.php
@@ -4,4 +4,5 @@
 	echo "File01 - version: Branch01 - status: Commited";
 	echo "File01 - version: Branch01 - status: Commited";
 	echo "File01 - version: Branch01 - status: Commited";
+	echo "File01 - version: master - status: Commited";
 ?>
