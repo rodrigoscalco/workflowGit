4d96457256c9c7eea76976d240c1e42f72825669
Rodrigo Scalco <rodrigo@bling.com.br>
2014-01-17 09:47:49 -0200

    partial commit.

--- File01.php
+++ File01.php
@@ -9,4 +9,5 @@
 	echo "File01 - version: remote master - status: Commited";
 	echo "File01 - version: remote master 2 - status: Commited";
 	echo "File01 - version: local master 2 - status: Stash";
+	echo "File01 - version: local master 3 - status: Commited";
 ?>
